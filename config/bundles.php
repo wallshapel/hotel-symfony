124380097671c64f0656cdde8b0e01d55bbf223f
Abel Camilo Yi Martínez <abelyimartinez1987@hotmail.com>
2025-03-20 14:58:30 -0500

    friendsofsymfony/rest-bundle for easy API

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
 ];
