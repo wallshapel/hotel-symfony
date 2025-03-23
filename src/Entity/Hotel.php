007f4b8870702f9e157b2d7213a0e485882dd7c9
Abel Camilo Yi Martínez <abelyimartinez1987@hotmail.com>
2025-03-22 21:33:09 -0500

    Bilateral relationship between Hotel and Room

--- src/Entity/Hotel.php
+++ src/Entity/Hotel.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\HotelRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -34,12 +36,12 @@ class Hotel
     #[Assert\NotBlank(message: "Description is required.")]
     private ?string $description = null;
 
-    #[ORM\Column]
-    private ?\DateTimeImmutable $createdAt = null;
+    #[ORM\OneToMany(mappedBy: 'hotel', targetEntity: Room::class, cascade: ['persist', 'remove'])]
+    private Collection $rooms;
 
     public function __construct()
     {
-        $this->createdAt = new \DateTimeImmutable();
+        $this->rooms = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -102,14 +104,32 @@ class Hotel
         return $this;
     }
 
-    public function getCreatedAt(): ?\DateTimeImmutable
+    /**
+     * @return Collection<int, Room>
+     */
+    public function getRooms(): Collection
     {
-        return $this->createdAt;
+        return $this->rooms;
     }
 
-    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    public function addRoom(Room $room): static
     {
-        $this->createdAt = $createdAt;
+        if (!$this->rooms->contains($room)) {
+            $this->rooms->add($room);
+            $room->setHotel($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRoom(Room $room): static
+    {
+        if ($this->rooms->removeElement($room)) {
+            if ($room->getHotel() === $this) {
+                $room->setHotel(null);
+            }
+        }
+
         return $this;
     }
 }
